234bd044fe038e2565d93d42226dada49f7b7605
t1st3 <contact@tiste.org>
2017-04-11 17:46:26 +0200

    feat: (breaking) es2015ify
    * Bump deps
    * use ES2015

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 'use strict';
 
-var isCinnamon = require('is-cinnamon');
-var isGnome = require('is-gnome');
-var isKde = require('is-kde');
-var isLxde = require('is-lxde');
-var isMate = require('is-mate');
-var isSugar = require('is-sugar');
-var isUnity = require('is-unity');
-var isXfce = require('is-xfce');
-var pify = require('pify');
+const isCinnamon = require('is-cinnamon');
+const isGnome = require('is-gnome');
+const isKde = require('is-kde');
+const isLxde = require('is-lxde');
+const isMate = require('is-mate');
+const isSugar = require('is-sugar');
+const isUnity = require('is-unity');
+const isXfce = require('is-xfce');
+const pify = require('pify');
 
-module.exports = pify(function (cb) {
+module.exports = pify(cb => {
 	if (process.platform === 'linux' || process.platform === 'freebsd') {
-		var de = 'N/A';
-		var desktopChecks = [isCinnamon(), isGnome(), isKde(), isLxde(), isMate(), isSugar(), isUnity(), isXfce()];
+		let de = 'N/A';
+		const desktopChecks = [isCinnamon(), isGnome(), isKde(), isLxde(), isMate(), isSugar(), isUnity(), isXfce()];
 
-		Promise.all(desktopChecks).then(function (values) {
-			var desktopNames = ['Cinnamon', 'Gnome', 'KDE', 'LXDE', 'Mate', 'Sugar', 'Unity', 'XFCE'];
-			values.map(function (v, k) {
+		Promise.all(desktopChecks).then(values => {
+			const desktopNames = ['Cinnamon', 'Gnome', 'KDE', 'LXDE', 'Mate', 'Sugar', 'Unity', 'XFCE'];
+			values.map((v, k) => {
 				if (v === true) {
 					de = desktopNames[k];
 				}
 				return de;
 			});
 			cb(null, de);
-		}).catch(function (err) {
+		}).catch(err => {
 			cb(err, null);
 		});
 	}
