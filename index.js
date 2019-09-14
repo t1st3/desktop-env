7e8333ae1565ce2397f12c3d1c0f25f3b8894613
t1st3 <contact@t1st3.com>
2019-09-15 00:37:18 +0200

    chore: update deps
    * update dependencies & dev dependencies
    * deps now use promises
    * v3.0.1

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ const pify = require('pify');
 module.exports = pify(cb => {
 	if (process.platform === 'linux' || process.platform === 'freebsd') {
 		let de = 'N/A';
-		const desktopChecks = [isCinnamon(), isGnome(), isKde(), isLxde(), isMate(), isSugar(), isUnity(), isXfce()];
+		const desktopChecks = [isCinnamon, isGnome, isKde, isLxde, isMate, isSugar, isUnity, isXfce];
 
-		Promise.all(desktopChecks).then(values => {
+		Promise.all(desktopChecks).then(values => { // eslint-disable-line promise/prefer-await-to-then
 			const desktopNames = ['Cinnamon', 'Gnome', 'KDE', 'LXDE', 'Mate', 'Sugar', 'Unity', 'XFCE'];
 			values.map((v, k) => {
 				if (v === true) {
 					de = desktopNames[k];
 				}
+
 				return de;
 			});
+
 			cb(null, de);
 		}).catch(error => {
 			cb(error, null);
